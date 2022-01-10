cdc40ad48f0246f2ddb117724fbee66c0d5eb3f9
mkrva-a <90410162+mkrva-a@users.noreply.github.com>
2022-01-10 14:17:39 +0500

    Лабораторная работа №5

---
+++ lab5/ChatHub.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+
+namespace lab4
+{
+    public class ChatHub : Hub
+    {
+        private readonly ILogger<ChatHub> log;
+
+        public ChatHub(ILogger<ChatHub> logger)
+        {
+            log = logger;
+        }
+
+
+        public async Task Send(IAsyncEnumerable<string> stream)
+        {
+            string outs = "";
+            await foreach (var item in stream)
+            {
+                log.LogInformation($"клиент отправил: {item}");
+                outs += item;
+            }
+
+
+            await Clients.All.SendAsync("Receive", outs);
+        }
+
+
+        public async IAsyncEnumerable<string> ReceiveHub(
+            string s,
+            int delay,
+            [EnumeratorCancellation]
+            CancellationToken cancellationToken)
+        {
+            
+                cancellationToken.ThrowIfCancellationRequested();
+                yield return s+"!";
+                await Task.Delay(delay, cancellationToken);
+
+   
+
+        }
+    }
+}
